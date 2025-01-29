afdf7d570b49b714a7d783a1e3eca364c90779df
Jia Jie Liang <dongfangjixuezhong@gmail.com>
2025-01-29 17:27:16 +0800

    新增 Initialization 部分，使其繼承 jsonControllerInitialization

--- src/main/java/com/example/initialization/JsonInitialization.java
+++ src/main/java/com/example/initialization/JsonInitialization.java
@@ -2,19 +2,17 @@ package com.example.initialization;
 
 import java.io.IOException;
 
-import com.example.jsonController.JsonController;
+import com.example.jsonControllerInitialization.JsonControllerInit;
 import com.example.jsonFormat.Account;
 import com.example.jsonFormat.PasswordsBackUp;
 import com.fasterxml.jackson.core.exc.StreamWriteException;
 import com.fasterxml.jackson.databind.DatabindException;
 
-public class JsonInitialization {
+public class JsonInitialization extends JsonControllerInit {
 
-    private final String ACCOUNT_PATH = "json_data/account.json";
-    private final String PASSWORDS_BACKUP_PATH = "json_data/passwords_backup.json";
-    private final JsonController JSON_CONTROLLER = new JsonController();
-
-    public JsonInitialization() {}
+    public JsonInitialization() {
+        super();
+    }
 
     public void account_json_setup(String user, String password) throws StreamWriteException, DatabindException, IOException {
         Account account = new Account();
